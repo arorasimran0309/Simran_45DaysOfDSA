23f796b5281f43acfdee7786e195a17d045fe2c0
Simran Arora <82223479+arorasimran0309@users.noreply.github.com>
2022-07-12 14:27:24 +0530

    Create 15. 3Sum.cpp

---
+++ 15. 3Sum.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> result;
+        
+        for(int i=0; i<nums.size()-2; i++)
+        {
+            if(i==0 || (i>0 && nums[i]!=nums[i-1]))
+            {
+                int low = i+1, high = nums.size()-1, sum = -nums[i];
+                
+                while(low < high)
+                {
+                    if(nums[low] + nums[high] == sum)
+                    {
+                        vector<int> tmp;
+                        tmp.push_back(nums[i]);
+                        tmp.push_back(nums[low]);
+                        tmp.push_back(nums[high]);
+                        result.push_back(tmp);
+                        
+                        while(low<high && nums[low] == nums[low+1]) low++;
+                        while(low<high && nums[high] == nums[high-1]) high--;
+                        
+                        low++; high--;
+                    }
+                    
+                    else if(nums[low] + nums[high] < sum)
+                        low++;
+                    else 
+                        high--;
+                }
+            }
+        }
+        return result;
+    }
+};
