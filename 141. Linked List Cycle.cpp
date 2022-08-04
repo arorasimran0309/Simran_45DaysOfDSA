949391f45b7ad9ce09a4d8165994d674604cf133
Simran Arora <82223479+arorasimran0309@users.noreply.github.com>
2022-08-05 00:10:30 +0530

    Create 141. Linked List Cycle.cpp

---
+++ 141. Linked List Cycle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head == NULL)
+            return false;
+        
+        ListNode *slow = head;
+        ListNode *fast = head;
+        
+        while(fast->next!=NULL && fast->next->next!=NULL)
+        {
+            fast=fast->next->next;
+            slow=slow->next;
+            if(slow==fast)
+                return true;
+        }
+        return false;
+    }
+};
